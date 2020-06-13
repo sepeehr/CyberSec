9062ef802bd3d14ce9eea4ccb6e4c08c650aa583
Sepehr Mohseni <sepcodes@gmail.com>
2020-06-13 16:24:41 +0430

    Create pass_creator.py

---
+++ PasswordGenerator/pass_creator.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+def GeneratePassword (passLength):
+    password = string.ascii_letters + string.digits + "!@#$%^&*()_+=-./?><|\}{[]"
+    #generating characters, numbers and letters for password
+    passwordList = []
+    #password list is for putting all the selected password units into a string and show them to user
+    for passChar in range(passLength):
+        passRandom = random.choice(password)
+        passwordList.append(passRandom)
+
+    finalOutput = "".join(passwordList)
+    return finalOutput
